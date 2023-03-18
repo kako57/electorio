ae7fec6c98ca1f4d96fb19bd689fa4417f74f1a4
kako57 <71569154+kako57@users.noreply.github.com>
2023-03-18 17:13:01 -0400

    kinda broken

---
+++ contracts/Poll.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol";
+
+contract Poll {
+  struct Candidate {
+    string name;
+    uint256 voteCount;
+  }
+
+  // we want the list of voters (an array of addresses)
+  address[] public voters;
+  // we want a list of candidates (an array of Candidate structs)
+  Candidate[] public candidates;
+
+  // we want a mapping of voters to booleans
+  mapping (address => bool) public hasVoted;
+
+  function addCandidate(string memory _name) public {
+    candidates.push(Candidate(_name, 0));
+  }
+
+  function addVote(uint256 _candidateIndex) public {
+    // check if the voter has already voted
+    require(!hasVoted[msg.sender], "Voter has already voted");
+
+    // check if the candidate index is valid
+    require(_candidateIndex < candidates.length, "Invalid candidate index");
+    
+    // add the voter to the list of voters
+    voters.push(msg.sender);
+
+    // update the voter's voting status to true
+    hasVoted[msg.sender] = true;
+
+    // update the candidate's vote count
+    candidates[_candidateIndex].voteCount++;
+  }
+
+  function getVoteCount(uint256 _candidateIndex) public view returns (uint256) {
+    // check if the candidate index is valid
+    require(_candidateIndex < candidates.length, "Invalid candidate index");
+
+    // return the vote count for the candidate
+    return candidates[_candidateIndex].voteCount;
+  }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,13 +12,13 @@ async function main() {
 
   const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Poll = await hre.ethers.getContractFactory("Poll");
+  const poll = await Poll.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  await poll.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(
+    `Poll with ${ethers.utils.formatEther(
       lockedAmount
     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
   );
